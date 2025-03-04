d6e3d87f6ede1c5d484d2b76434643ee2ebada3a
hyperkubo <hyperkubo@outlook.com>
2025-03-04 12:26:33 -0600

    Agrega la utilería para manejar archivos
    Con esta utilería se pretende abrir y leer el archivo que será el input
    de los diferentes ejercicios.

---
+++ AdventOfCode/Utils/FileManager.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.Utils
+{
+    public class FileManager : IFileManager
+    {
+        public List<string> GetLines(string path)
+        {
+            List<string> result = [];
+            StreamReader sr = new(path);
+            string? line = sr.ReadLine();
+            while (line != null)
+            {
+                result.Add(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode/Utils/IFileManager.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode.Utils
+{
+    public interface IFileManager
+    {
+        abstract List<string> GetLines(string path);
+    }
+}
