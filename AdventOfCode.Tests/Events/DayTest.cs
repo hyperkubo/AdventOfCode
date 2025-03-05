7b983f6c15bd0cac1f4254e843a7da15646c75c4
hyperkubo <hyperkubo@outlook.com>
2025-03-05 17:28:29 -0600

    Agrega el proyecto para tests
    Contiene una clase base de donde heredarán todos las demás clases para
    tests.

---
+++ AdventOfCode.Tests/Events/DayTest.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Events;
+using AdventOfCode.Utils;
+using Moq;
+
+namespace AdventOfCode.Tests.Events
+{
+    abstract class DayTest<T> where T : Day
+    {
+        protected Mock<IFileManager> FileManager { get; private set; }
+        protected T Day { get; private set; }
+        protected List<string> InputLines { get; set; } = [];
+
+        [SetUp]
+        public void SetUp()
+        {
+            FileManager = new Mock<IFileManager>();
+            Day = (T)(Activator.CreateInstance(typeof(T), FileManager.Object) ?? new object());
+        }
+
+        public abstract void Part1();
+        public abstract void Part2();
+    }
+}
--- AdventOfCode/Events/Day.cs
+++ AdventOfCode/Events/Day.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode.Events
 {
     public abstract class Day(IFileManager? fileManager) : IDay
     {
-        protected readonly IFileManager _fileManager = fileManager ?? new FileManager();
+        protected IFileManager FileManager { get; } = fileManager ?? new FileManager();
 
         public abstract int Part1(string inputFilePath);
         public abstract int Part2(string inputFilePath);
