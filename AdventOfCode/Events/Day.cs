aee55d1ddad968794285699e9ff1b73925415b85
hyperkubo <hyperkubo@outlook.com>
2025-03-05 10:48:33 -0600

    Crea la estructura de los ejercicios
    Crea una clase abstracta de la cuál tendrán que derivar todos los
    ejercicios.

---
+++ AdventOfCode/Events/Day.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode.Events
+{
+    public abstract class Day(IFileManager? fileManager) : IDay
+    {
+        protected readonly IFileManager _fileManager = fileManager ?? new FileManager();
+
+        public abstract int Part1(string inputFilePath);
+        public abstract int Part2(string inputFilePath);
+    }
+}
---
+++ AdventOfCode/Events/IDay.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Events
+{
+    interface IDay
+    {
+        public abstract int Part1(string inputFilePath);
+        public abstract int Part2(string inputFilePath);
+    }
+}
