87443629d6ad4b6f99a41ab75c233e5cdb09a93e
hyperkubo <hyperkubo@outlook.com>
2025-03-06 14:37:03 -0600

    Resuelve Año 2024 Día 1 Parte 1
    --- Day 1: Historian Hysteria ---

--- AdventOfCode/Events/2024/Day01.cs
+++ AdventOfCode/Events/2024/Day01.cs
@@ -7,7 +7,19 @@ namespace AdventOfCode.Events.Year2024
     {
         public override int Part1(string inputFilePath)
         {
-            throw new NotImplementedException();
+            int result = 0;
+
+            var inputLines = FileManager.GetLines(inputFilePath);
+
+            var left = inputLines.Select(l => Convert.ToInt32(l.Split(' ').First())).OrderBy(a => a);
+            var right = inputLines.Select(l => Convert.ToInt32(l.Split(' ').Last())).OrderBy(a => a);
+
+            foreach (var (l, r) in left.Zip(right, (l, r) => (l, r)))
+            {
+                result += Math.Abs(l - r);
+            }
+
+            return result;
         }
 
         public override int Part2(string inputFilePath)
