7548f3c33e13af8bbc5ed06131a8dbc18e327a0a
hyperkubo <hyperkubo@outlook.com>
2025-03-06 14:31:37 -0600

    Agrega test para Año 2024 Dia 1 Parte 1

---
+++ AdventOfCode.Tests/Events/2024/Day01Tests.cs
@@ -0,0 +1,36 @@
+using AdventOfCode.Events.Year2024;
+using Moq;
+
+namespace AdventOfCode.Tests.Events.Year2024
+{
+    class Day01Tests : DayTest<Day01>
+    {
+        public Day01Tests()
+        {
+            InputLines = [
+                "3   4",
+                "4   3",
+                "2   5",
+                "1   3",
+                "3   9",
+                "3   3"
+            ];
+        }
+
+        [Test]
+        public override void Part1()
+        {
+            FileManager.Setup(f => f.GetLines(It.IsAny<string>())).Returns(() => InputLines);
+
+            var output = Day.Part1(It.IsAny<string>());
+
+            Assert.That(output, Is.EqualTo(11));
+        }
+
+        [Test]
+        [Ignore("Working on it")]
+        public override void Part2()
+        {
+        }
+    }
+}
---
+++ AdventOfCode/Events/2024/Day01.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode.Events.Year2024
+{
+    //--- Day 1: Historian Hysteria ---
+    public class Day01(IFileManager? fileManager = null) : Day(fileManager)
+    {
+        public override int Part1(string inputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Part2(string inputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
