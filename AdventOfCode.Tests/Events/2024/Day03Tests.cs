b6079cf09537b1867f1c6aa27c6ac9e53bd45f85
hyperkubo <hyperkubo@outlook.com>
2025-03-10 12:35:09 -0600

    Agrega test Año 2024 Día 3 Parte 2

--- AdventOfCode.Tests/Events/2024/Day03Tests.cs
+++ AdventOfCode.Tests/Events/2024/Day03Tests.cs
@@ -5,23 +5,24 @@ namespace AdventOfCode.Tests.Events.Year2024
 {
     class Day03Tests : DayTest<Day03>
     {
-        public Day03Tests()
-        {
-            InputLines = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
-        }
-
         [Test]
         public override void Part1()
         {
+            InputLines = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
+
             var result = Day.Part1(It.IsAny<string>());
 
             Assert.That(result, Is.EqualTo(161));
         }
 
         [Test]
-        [Ignore("Maybe next commit...")]
         public override void Part2()
         {
+            InputLines = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"];
+
+            var result = Day.Part2(It.IsAny<string>());
+
+            Assert.That(result, Is.EqualTo(48));
         }
     }
 }
