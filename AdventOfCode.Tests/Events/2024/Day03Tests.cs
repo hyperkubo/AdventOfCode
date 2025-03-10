ecc789cfc99bda65e98956d046837fa56ecd017a
hyperkubo <hyperkubo@outlook.com>
2025-03-10 11:52:10 -0600

    Agrega test Año 2024 Día 3 Parte 1

---
+++ AdventOfCode.Tests/Events/2024/Day03Tests.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Events.Year2024;
+using Moq;
+
+namespace AdventOfCode.Tests.Events.Year2024
+{
+    class Day03Tests : DayTest<Day03>
+    {
+        public Day03Tests()
+        {
+            InputLines = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
+        }
+
+        [Test]
+        public override void Part1()
+        {
+            var result = Day.Part1(It.IsAny<string>());
+
+            Assert.That(result, Is.EqualTo(161));
+        }
+
+        [Test]
+        [Ignore("Maybe next commit...")]
+        public override void Part2()
+        {
+        }
+    }
+}
---
+++ AdventOfCode/Events/2024/Day03.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode.Events.Year2024
+{
+    //--- Day 3: Mull It Over ---
+    public class Day03(IFileManager? fileManager = null) : Day(fileManager)
+    {
+        public override int Part1(string inputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Part2(string inputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
