0de2fa10e85729777b4b9260a11993381d1ab551
hyperkubo <hyperkubo@outlook.com>
2025-03-10 10:06:43 -0600

    Resuelve Año 2024 Día 2 Parte 1
    --- Day 2: Red-Nosed Reports ---

--- AdventOfCode/Events/2024/Day02.cs
+++ AdventOfCode/Events/2024/Day02.cs
@@ -7,13 +7,31 @@ namespace AdventOfCode.Events.Year2024
     {
         public override int Part1(string inputFilePath)
         {
-            throw new NotImplementedException();
-        {
-}
+            var inputLines = FileManager.GetLines(inputFilePath);
+            var reports = inputLines.Select(l => l.Split(' ').Select(n => Convert.ToInt32(n)));
+
+            return reports.Count(report => IsSafe([..report]));
+        }
 
         public override int Part2(string inputFilePath)
         {
             throw new NotImplementedException();
         }
+
+        #region Private helper methods
+        private static bool IsSafe(int[] report)
+        {
+            int[] diffs = new int[report.Length - 1];
+            for (int i = 0; i < report.Length - 1; i++)
+            {
+                diffs[i] = report[i] - report[i + 1];
+            }
+            if (diffs.Any(d => d >= 0) && diffs.Any(d => d <= 0)){
+                return false;
+            }
+
+            return diffs.All(d => Math.Abs(d) is 1 or 2 or 3);
+        }
+        #endregion
     }
 }
