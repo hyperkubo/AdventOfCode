bc1fba66172ae6fe4c451e4751143a553f452d8a
hyperkubo <hyperkubo@outlook.com>
2025-03-11 14:08:10 -0600

    Resuelve Año 2024 Día 4 Parte 1
    --- Day 4: Ceres Search ---

--- AdventOfCode/Events/2024/Day04.cs
+++ AdventOfCode/Events/2024/Day04.cs
@@ -1,18 +1,137 @@
-using AdventOfCode.Utils;
+﻿using AdventOfCode.Utils;
+using System.Text;
 
 namespace AdventOfCode.Events.Year2024
 {
     //--- Day 4: Ceres Search ---
     public class Day04(IFileManager? fileManager = null) : Day(fileManager)
     {
+        static Day04()
+        {
+            var theWordArr = _magicWord.ToCharArray();
+            Array.Reverse(theWordArr);
+            _reversedMagicWord = new string(theWordArr);
+        }
+
+        private static readonly string _magicWord = "XMAS";
+        private static readonly string _reversedMagicWord;
+
         public override int Part1(string inputFilePath)
         {
-            throw new NotImplementedException();
+            int xmasCounter = 0;
+
+            var inputLines = FileManager.GetLines(inputFilePath);
+
+            xmasCounter += MagicWordOcurrences(inputLines);
+            xmasCounter += MagicWordOcurrences(VerticallyRotate(inputLines));
+            xmasCounter += MagicWordOcurrences(ClockwiseRotate(inputLines));
+            xmasCounter += MagicWordOcurrences(CounterClockwiseRotate(inputLines));
+
+            return xmasCounter;
         }
 
         public override int Part2(string inputFilePath)
         {
             throw new NotImplementedException();
         }
+
+        #region Private helpers
+        private static int MagicWordOcurrences(List<string> myCrossword)
+        {
+            int magicWordCounter = 0;
+
+            magicWordCounter += myCrossword.Sum(line => line.Split(_magicWord).Length - 1);
+            magicWordCounter += myCrossword.Sum(line => line.Split(_reversedMagicWord).Length - 1);
+
+            return magicWordCounter;
+        }
+
+        private static List<string> VerticallyRotate(List<string> myCrossword)
+        {
+            var rotatedCrossword = new List<string>();
+            StringBuilder myNewLine;
+
+            for (int col = 0; col < myCrossword.First().Length; col++)
+            {
+                myNewLine = new();
+                for (int row = 0; row < myCrossword.Count; row++)
+                {
+                    myNewLine.Append(myCrossword[row][col]);
+                }
+                rotatedCrossword.Add(myNewLine.ToString());
+            }
+            return rotatedCrossword;
+        }
+
+        private static List<string> ClockwiseRotate(List<string> myCrossword)
+        {
+            var diagonallyClockwiseRotatedLinesArr = new List<string>();
+            var totalCols = myCrossword.First().Length;
+            var totalRows = myCrossword.Count;
+            StringBuilder myNewLine;
+            for (int i = totalCols - _magicWord.Length; i >= 0; i--)
+            {
+                int col = i;
+                int row = 0;
+                myNewLine = new();
+                while (col < totalCols)
+                {
+                    myNewLine.Append(myCrossword[row][col]);
+                    row++;
+                    col++;
+                }
+                diagonallyClockwiseRotatedLinesArr.Add(myNewLine.ToString());
+            }
+            for (int i = 1; i <= totalRows - _magicWord.Length; i++)
+            {
+                int row = i;
+                int col = 0;
+                myNewLine = new StringBuilder();
+                while (row < totalRows)
+                {
+                    myNewLine.Append(myCrossword[row][col]);
+                    row++;
+                    col++;
+                }
+                diagonallyClockwiseRotatedLinesArr.Add(myNewLine.ToString());
+            }
+            return diagonallyClockwiseRotatedLinesArr;
+        }
+
+        private static List<string> CounterClockwiseRotate(List<string> myCrossword)
+        {
+            var diagonallyCounterClockwiseRotatedLinesArr = new List<string>();
+            var totalCols = myCrossword.First().Length;
+            var totalRows = myCrossword.Count;
+            StringBuilder myNewLine;
+            for(int i = _magicWord.Length - 1; i < totalRows; i++)
+            {
+                int row = i;
+                int col = 0;
+                myNewLine = new ();
+                while(row >= 0)
+                {
+                    myNewLine.Append(myCrossword[row][col]);
+                    row--;
+                    col++;
+                }
+                diagonallyCounterClockwiseRotatedLinesArr.Add(myNewLine.ToString());
+            }
+            for(int i = 1; i <= totalCols - _magicWord.Length; i++)
+            {
+                int col = i;
+                int row = totalRows - 1;
+                myNewLine = new ();
+                while(col < totalCols)
+                {
+                    myNewLine.Append(myCrossword[row][col]);
+                    row--;
+                    col++;
+                }
+                diagonallyCounterClockwiseRotatedLinesArr.Add(myNewLine.ToString());
+            }
+            return diagonallyCounterClockwiseRotatedLinesArr;
+        }
+        #endregion
     }
 }
