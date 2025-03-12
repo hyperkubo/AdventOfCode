bb6cebac45b8b8123c8118bb566e256e9f5ddf02
hyperkubo <hyperkubo@outlook.com>
2025-03-12 10:30:41 -0600

    Resuelve Año 2024 Día 5 Parte 1
    --- Day 5: Print Queue ---

--- AdventOfCode/Events/2024/Day05.cs
+++ AdventOfCode/Events/2024/Day05.cs
@@ -7,7 +7,31 @@ namespace AdventOfCode.Events.Year2024
     {
         public override int Part1()
         {
-            throw new NotImplementedException();
+            var rules = InputLines.Where(line => line.Contains('|'));
+            var updates = InputLines.Where(line => line.Contains(','));
+
+            List<string> updatesInOrder = [];
+            foreach (var update in updates)
+            {
+                bool validUpdate = false;
+                var updatePages = update.Split(',');
+                for (int i = 0; i < updatePages.Length - 1; i++)
+                {
+                    validUpdate = rules.Any(rule => rule == $"{updatePages[i]}|{updatePages[i + 1]}");
+                    if (!validUpdate) break;
+                }
+                if (validUpdate)
+                {
+                    updatesInOrder.Add(update);
+                }
+            }
+
+            return updatesInOrder.Sum(u =>
+            {
+                var pagesInUpdate = u.Split(',');
+                var middleIdx = (int)Math.Ceiling(pagesInUpdate.Length / 2.0) - 1;
+                return Convert.ToInt32(pagesInUpdate[middleIdx]);
+            });
         }
 
         public override int Part2()
