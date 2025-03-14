7a06339f9cb8f2e06e0c87cac55a5c3f20775f1e
hyperkubo <hyperkubo@outlook.com>
2025-03-14 10:47:53 -0600

    Agrega test Año 2024 Día 5 Parte 2

--- AdventOfCode.Tests/Events/2024/Day05Tests.cs
+++ AdventOfCode.Tests/Events/2024/Day05Tests.cs
@@ -47,9 +47,11 @@ namespace AdventOfCode.Tests.Events.Year2024
         }
 
         [Test]
-        [Ignore("What did I say before...?")]
         public override void Part2()
         {
+            var result = Day.Part2();
+
+            Assert.That(result, Is.EqualTo(123));
         }
     }
 }
