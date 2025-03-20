e3e3c8b0ab5d9798252bc539c18d44c84cff4bb5
hyperkubo <hyperkubo@outlook.com>
2025-03-20 12:55:12 -0600

    Resuelve Año 2024 Día 7 Parte 2
    --- Day 7: Bridge Repair ---

--- AdventOfCode/Events/2024/Day07.cs
+++ AdventOfCode/Events/2024/Day07.cs
@@ -3,27 +3,101 @@ using AdventOfCode.Utils;
 namespace AdventOfCode.Events.Year2024
 {
     //--- Day 7: Bridge Repair ---
-    public class Day07(string inputFilePath, IFileManager? fileManager = null) : Day(inputFilePath, fileManager)
+    public class Day07 : Day
     {
-        public override long Part1()
+        private int _operatorsToTake;
+        private readonly IEnumerable<(long TestValue, IEnumerable<long> Operands)> _data;
+        private readonly Dictionary <int, List<List<Operators>>> _operatorsCombinationsDirectory = [];
+        public Day07(string inputFilePath, IFileManager? fileManager = null) : base(inputFilePath, fileManager)
         {
-            var data = InputLines.Select(l =>
+            _data = InputLines.Select(l =>
             {
                 var testAndOperands = l.Split(':');
                 var testValue = Convert.ToInt64(testAndOperands[0]);
                 var operands = testAndOperands[1].Split(' ').Where(c => !string.IsNullOrEmpty(c)).Select(o => Convert.ToInt64(o));
                 return (TestValue: Convert.ToInt64(testAndOperands[0]), Operands: operands);
             });
+        }
+        public override long Part1()
+        {
+            return SumOfTrueEquations(2);
+        }
 
-            long sumOfTrueEquations = 0;
-            foreach (var (TestValue, Operands) in data)
+        public override long Part2()
+        {
+            return SumOfTrueEquations(3);
+        }
+
+        #region Private helpers
+        private static long Calc(List<long> operands, List<Operators> operators)
+        {
+            while(operands.Count > 1)
             {
-                var operationsQty = Operands.Count() - 1;
-                var operatorsList = Operators(operationsQty);
+                var @operator = operators.First();
+                long operationResult = @operator switch
+                {
+                    Operators.Add => operands.ElementAt(0) + operands.ElementAt(1),
+                    Operators.Multiply => operands.ElementAt(0) * operands.ElementAt(1),
+                    Operators.Concat => Convert.ToInt64($"{operands.ElementAt(0)}{operands.ElementAt(1)}"),
+                    _ => 0
+                };
+                operands.RemoveAt(0);
+                operands.RemoveAt(0);
+                operators.RemoveAt(0);
+                operands.Insert(0, operationResult);
+            }
+            return operands.Single();
+        }
+
+        private List<List<Operators>> OperatorsCombinations(int operandsQty)
+        {
+            if (_operatorsCombinationsDirectory.TryGetValue(operandsQty, out List<List<Operators>>? value))
+            {
+                return value;
+            }
+            else
+            {
+                int operatorsQty = operandsQty - 1;
+                int totalPermutations = (int)Math.Pow(_operatorsToTake, operatorsQty) - 1;
+
+                List<List<int>> operatorsList = [];
+                for(int i = 0; i <= totalPermutations; i++)
+                {
+                    operatorsList.Add(ConvertToBase(i, _operatorsToTake, operatorsQty));
+                }
+
+                var operators = operatorsList.Select(l => l.Select(v => (Operators)v).ToList()).ToList();
+                _operatorsCombinationsDirectory.Add(operandsQty, operators);
+                return operators;
+            }
+        }
+
+        private static List<int> ConvertToBase(int myNum, int @base, int positions)
+        {
+           List<int> baseNums = [];
+            while(myNum > 0)
+            {
+                baseNums.Add(myNum % @base);
+                myNum /= @base;
+            }
+
+            baseNums.AddRange(Enumerable.Repeat(0, positions - baseNums.Count));
+
+            baseNums.Reverse();
+            return baseNums;
+        }
+
+        private long SumOfTrueEquations(int operatorsToTake)
+        {
+            _operatorsToTake = operatorsToTake;
+            long sumOfTrueEquations = 0;
+            foreach (var (TestValue, Operands) in _data)
+            {
+                var operatorsList = OperatorsCombinations(Operands.Count());
 
                 foreach(var operators in operatorsList)
                 {
-                    var result = Calc([.. Operands], operators);
+                    var result = Calc([.. Operands], [.. operators]);
                     if (result == TestValue)
                     {
                         sumOfTrueEquations += TestValue;
@@ -34,44 +108,11 @@ namespace AdventOfCode.Events.Year2024
             return sumOfTrueEquations;
         }
 
-        public override long Part2()
+        private enum Operators
         {
-            throw new NotImplementedException();
-        }
-
-        #region Private helpers
-        private static long Calc(List<long> operands, List<char> operators)
-        {
-            if (operands.Count == 1) return operands.Single();
-
-            var @operator = operators.First();
-            long operationResult = @operator switch
-            {
-                '+' => operands.ElementAt(0) + operands.ElementAt(1),
-                '*' => operands.ElementAt(0) * operands.ElementAt(1),
-                _ => 0
-            };
-            operands.RemoveAt(0);
-            operands.RemoveAt(0);
-            operators.RemoveAt(0);
-            operands.Insert(0, operationResult);
-
-            return Calc(operands, operators);
-        }
-
-        private static List<List<char>> Operators(int totalOperations)
-        {
-            return [.. Enumerable.Range(0, (int)Math.Pow(2, totalOperations))
-                .Select(n => n.ToString("b").PadLeft(totalOperations, '0'))
-                .Select(b => b.Select(c => c switch
-                {
-                    '0' => '+',
-                    '1' => '*',
-                    _ => throw new Exception()
-                }).ToList())
-            ];
-
-            throw new NotImplementedException();
+            Add,
+            Multiply,
+            Concat
         }
         #endregion
     }
