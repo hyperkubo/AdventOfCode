6638c9afddd553d63f96de8e4fcd53d2088d9628
hyperkubo <hyperkubo@outlook.com>
2025-04-22 13:48:27 -0600

    Agrega test Año 2024 Día 10 Parte 1

---
+++ AdventOfCode.Tests/Events/2024/Day10Tests.cs
@@ -0,0 +1,34 @@
+using AdventOfCode.Events.Year2024;
+
+namespace AdventOfCode.Tests.Events.Year2024
+{
+    class Day10Tests : DayTest<Day10>
+    {
+        public Day10Tests()
+        {
+            InputLines = [
+               "89010123",
+               "78121874",
+               "87430965",
+               "96549874",
+               "45678903",
+               "32019012",
+               "01329801",
+               "10456732"
+            ];
+        }
+
+        [Test]
+        public override void Part1()
+        {
+            var result = Day.Part1();
+
+            Assert.That(result, Is.EqualTo(36));
+        }
+
+        public override void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode/Events/2024/Day10.cs
@@ -0,0 +1,18 @@
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Events.Year2024
+{
+    //--- Day 10: Hoof It ---
+    public class Day10(string inputFilePath, IFileManager? fileManager = null) : Day(inputFilePath, fileManager)
+    {
+        public override long Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
