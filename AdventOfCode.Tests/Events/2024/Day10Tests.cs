b3428b3e9e5eca9fc339152e5f7c82d7a4dae8e2
hyperkubo <hyperkubo@outlook.com>
2025-04-23 12:33:47 -0600

    Agrega test Año 2024 Día 10 Parte 2

--- AdventOfCode.Tests/Events/2024/Day10Tests.cs
+++ AdventOfCode.Tests/Events/2024/Day10Tests.cs
@@ -26,9 +26,12 @@ namespace AdventOfCode.Tests.Events.Year2024
             Assert.That(result, Is.EqualTo(36));
         }
 
+        [Test]
         public override void Part2()
         {
-            throw new NotImplementedException();
+            var result = Day.Part2();
+
+            Assert.That(result, Is.EqualTo(81));
         }
     }
 }
