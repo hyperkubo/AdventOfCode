9ae73822103cb9be383d877ff763a858a87423ba
hyperkubo <hyperkubo@outlook.com>
2025-04-23 12:24:17 -0600

    Resuelve Año 2024 Día 10 Parte 1
    --- Day 10: Hoof It ---

--- AdventOfCode/Events/2024/Day10.cs
+++ AdventOfCode/Events/2024/Day10.cs
@@ -5,14 +5,90 @@ namespace AdventOfCode.Events.Year2024
     //--- Day 10: Hoof It ---
     public class Day10(string inputFilePath, IFileManager? fileManager = null) : Day(inputFilePath, fileManager)
     {
+        private readonly char _trailStart = '0';
+        private readonly char _trailEnd = '9';
+        private readonly List<HikingTrail> _hikingHeads = [];
+        private int MaxRow => InputLines.Count - 1;
+        private int MaxCol => InputLines.First().Length - 1;
         public override long Part1()
         {
-            throw new NotImplementedException();
+            FindTrailheads();
+
+            foreach(var hikingHead in _hikingHeads)
+            {
+                GetNextHikingTrailSteps(hikingHead);
+            }
+
+            return _hikingHeads.Sum(h => h.Score);
         }
 
         public override long Part2()
         {
             throw new NotImplementedException();
         }
+
+        #region Private methods
+        private void FindTrailheads()
+        {
+            for(int row = 0; row <= MaxRow; row++)
+            {
+                for(int col = 0; col <= MaxCol; col++)
+                {
+                    if (InputLines[row][col] == _trailStart)
+                    {
+                        _hikingHeads.Add(new(_trailStart, row, col));
+                    }
+                }
+            }
+        }
+
+        private void GetNextHikingTrailSteps(HikingTrail hikingTrail, HikingTrail? start = null)
+        {
+            start ??= hikingTrail;
+            if (hikingTrail.Height == _trailEnd)
+            {
+                if(!start.DifferentHikingEnds.Exists(h => h.Row == hikingTrail.Row && h.Col == hikingTrail.Col))
+                {
+                    start.DifferentHikingEnds.Add(hikingTrail);
+                }
+                return;
+            }
+            var nextCol = hikingTrail.Col + 1;
+            var prevCol = hikingTrail.Col - 1;
+            var nextRow = hikingTrail.Row + 1;
+            var prevRow = hikingTrail.Row - 1;
+
+            if(prevCol >= 0 && InputLines[hikingTrail.Row][prevCol] == hikingTrail.Height + 1)
+            {
+                hikingTrail.NextPositions.Add(new((char)(hikingTrail.Height + 1), hikingTrail.Row, prevCol));
+            }
+            if(nextCol <= MaxCol && InputLines[hikingTrail.Row][nextCol] == hikingTrail.Height + 1)
+            {
+                hikingTrail.NextPositions.Add(new((char)(hikingTrail.Height + 1), hikingTrail.Row, nextCol));
+            }
+            if(prevRow >= 0 && InputLines[prevRow][hikingTrail.Col] == hikingTrail.Height + 1)
+            {
+                hikingTrail.NextPositions.Add(new((char)(hikingTrail.Height + 1), prevRow, hikingTrail.Col));
+            }
+            if(nextRow <= MaxRow && InputLines[nextRow][hikingTrail.Col] == hikingTrail.Height + 1)
+            {
+                hikingTrail.NextPositions.Add(new((char)(hikingTrail.Height + 1), nextRow, hikingTrail.Col));
+            }
+            foreach(var nextStep in hikingTrail.NextPositions)
+            {
+                GetNextHikingTrailSteps(nextStep, start);
+            }
+        }
+
+        class HikingTrail(char height, int row, int col)
+        {
+            public char Height { get; set; } = height;
+            public int Row { get; set; } = row;
+            public int Col { get; set; } = col;
+            public int Score => DifferentHikingEnds.Count;
+            public List<HikingTrail> DifferentHikingEnds { get; set; } = [];
+            public List<HikingTrail> NextPositions { get; set; } = [];
+        }
+        #endregion
     }
 }
