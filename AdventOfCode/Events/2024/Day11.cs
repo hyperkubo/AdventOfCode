b3bca98c98f2920e5af00d0ec41f5f7a23ec2e42
hyperkubo <hyperkubo@outlook.com>
2025-04-24 14:42:28 -0600

    Modifica la lógica para el conteo
    --- Day 10: Plutonian Pebbles ---
    
    En esta prueba no cambia la parte uno de la parte dos, únicamente se
    itera más veces en la parte dos.
    Esta iteración extra hacía que fuera interminable la ejecución (haz la
    ejecución de la parte dos con la lógica que está al resolver la parte
    uno...).
    Así que se cambia la lógica de "una lista con las piedras" a "una lista
    de piedras con su contador".
    
    NOTA: Al no haber diferencia entre la parte uno y dos, no existe el test
    para la parte dos.

--- AdventOfCode/Events/2024/Day11.cs
+++ AdventOfCode/Events/2024/Day11.cs
@@ -3,49 +3,93 @@ using AdventOfCode.Utils;
 namespace AdventOfCode.Events.Year2024
 {
     //--- Day 11: Plutonian Pebbles ---
-    public class Day11(string inputFilePath, IFileManager? fileManager = null) : Day(inputFilePath, fileManager)
+    public class Day11 : Day
     {
-        private List<string> _stones = [];
+        private readonly Dictionary<string, long> _stonesCount = [];
+
+        public Day11(string inputFilePath, IFileManager? fileManager = null) : base(inputFilePath, fileManager)
+        {
+            _stonesCount = InputLines.First().Split(' ').Select(s => new KeyValuePair<string, long>(s, 1)).ToDictionary();
+        }
+
         public override long Part1()
         {
-            _stones = [.. InputLines.First().Split(' ')];
             Blink(25);
 
-            return _stones.Count();
+            return _stonesCount.Sum(s => s.Value);
         }
 
         public override long Part2()
         {
-            throw new NotImplementedException();
+            Blink(75);
+
+            return _stonesCount.Sum(s => s.Value);
         }
 
         #region Private methods
-        private void Blink(int times = 0)
+        private void Blink(int times = 1)
         {
-            List<string> newStonesLine = [];
-            foreach(var stone in _stones)
+            if (times < 0) throw new ArgumentException("How do you blink in a negative way...?");
+            for(int i = times; i > 0; i--)
             {
-                if(stone == "0")
+                var stonesInBlink = _stonesCount.Where(s => s.Value > 0).Select(s => s.Key).ToList();
+                Dictionary<string, long> newStonesCount = [];
+                foreach (var stone in stonesInBlink)
                 {
-                    newStonesLine.Add("1");
+                    if (stone == "0")
+                    {
+                        if (newStonesCount.ContainsKey("1")){
+                            newStonesCount["1"] += _stonesCount["0"];
+                        }
+                        else
+                        {
+                            newStonesCount.Add("1", _stonesCount["0"]);
+                        }
+                    }
+                    else if (stone.Length % 2 == 0)
+                    {
+                        var halfStoneLength = stone.Length / 2;
+                        var stone1 = Convert.ToInt32(stone[..halfStoneLength]).ToString();
+                        if (newStonesCount.ContainsKey(stone1)){
+                            newStonesCount[stone1] += _stonesCount[stone];
+                        }
+                        else
+                        {
+                            newStonesCount.Add(stone1, _stonesCount[stone]);
+                        }
+                        var stone2 = Convert.ToInt32(stone.Substring(halfStoneLength, halfStoneLength)).ToString();
+                        if (newStonesCount.ContainsKey(stone2)){
+                            newStonesCount[stone2] += _stonesCount[stone];
+                        }
+                        else
+                        {
+                            newStonesCount.Add(stone2, _stonesCount[stone]);
+                        }
+                    }
+                    else
+                    {
+                        var newStone = (Convert.ToInt64(stone) * 2024).ToString();
+                        if (newStonesCount.ContainsKey(newStone)){
+                            newStonesCount[newStone] += _stonesCount[stone];
+                        }
+                        else
+                        {
+                            newStonesCount.Add(newStone, _stonesCount[stone]);
+                        }
+                    }
+                    _stonesCount[stone] = 0;
                 }
-                else if(stone.Length % 2 == 0)
+                foreach (var stoneCount in newStonesCount)
                 {
-                    var halfStoneLength = stone.Length / 2;
-                    var stone1 = stone[..(halfStoneLength)];
-                    var stone2 = stone.Substring(halfStoneLength, halfStoneLength);
-                    newStonesLine.AddRange([stone1, Convert.ToInt64(stone2).ToString()]);
+                    if (_stonesCount.ContainsKey(stoneCount.Key))
+                    {
+                        _stonesCount[stoneCount.Key] = stoneCount.Value;
+                    }
+                    else
+                    {
+                        _stonesCount.Add(stoneCount.Key, stoneCount.Value);
+                    }
                 }
-                else
-                {
-                    newStonesLine.Add((Convert.ToInt64(stone) * 2024).ToString());
-                }
-            }
-            _stones = newStonesLine;
-
-            if(times > 1)
-            {
-                Blink(times - 1);
             }
         }
         #endregion
