a8192c757d4bfbf8a471b2593480c52909274b80
hyperkubo <hyperkubo@outlook.com>
2025-05-29 12:05:13 -0600

    Agrega test Año 2024 Día 15 Parte 1

---
+++ AdventOfCode.Tests/Events/2024/Day15Tests.cs
@@ -0,0 +1,65 @@
+using AdventOfCode.Events.Year2024;
+
+namespace AdventOfCode.Tests.Events.Year2024
+{
+    class Day15Tests : DayTest<Day15>
+    {
+        private static IEnumerable<TestCaseData> Inputs
+        {
+            get
+            {
+                yield return new TestCaseData(new List<string>
+                {
+                    "########",
+                    "#..O.O.#",
+                    "##@.O..#",
+                    "#...O..#",
+                    "#.#.O..#",
+                    "#...O..#",
+                    "#......#",
+                    "########",
+                    "",
+                    "<^^>>>vv<v>>v<<"
+                })
+                .Returns(2028);
+
+                yield return new TestCaseData(new List<string>
+                {
+                    "##########",
+                    "#..O..O.O#",
+                    "#......O.#",
+                    "#.OO..O.O#",
+                    "#..O@..O.#",
+                    "#O#..O...#",
+                    "#O..O..O.#",
+                    "#.OO.O.OO#",
+                    "#....O...#",
+                    "##########",
+                    "",
+                    "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
+                })
+                .Returns(10092);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(Inputs))]
+        public long Part1(List<string> inputLines)
+        {
+            InputLines = inputLines;
+            var result = Day.Part1();
+
+            return result;
+        }
+
+        public override void Part1()
+        {
+            // This method will be overloaded, and not be used, in order to accept multiples test cases.
+            throw new NotImplementedException();
+        }
+
+        public override void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode/Events/2024/Day15.cs
@@ -0,0 +1,18 @@
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Events.Year2024
+{
+    //--- Day 15: Warehouse Woes ---
+    public class Day15(string inputFilePath, IFileManager? fileManager = null) : Day(inputFilePath, fileManager)
+    {
+        public override long Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
