1b04dfc6ec9b948c09acc19af55bc00b37de1748
hyperkubo <hyperkubo@outlook.com>
2025-07-14 14:53:34 -0600

    Agrega test Año 2024 Día 16 Parte 1

---
+++ AdventOfCode/Events/2024/Day16.cs
@@ -0,0 +1,18 @@
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Events.Year2024
+{
+    //--- Day 16: Reindeer Maze ---
+    public class Day16(string inputFilePath, IFileManager? fileManager = null) : Day(inputFilePath, fileManager)
+    {
+        public override long Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
