5de934d68c6bab5d9aa8b368c48e85d04e78d46b
hyperkubo <hyperkubo@outlook.com>
2025-07-14 10:12:54 -0600

    Resuelve Año 2024 Día 15 Parte 2
    --- Day 15: Warehouse Woes ---

--- AdventOfCode/Events/2024/Day15.cs
+++ AdventOfCode/Events/2024/Day15.cs
@@ -10,11 +10,13 @@ namespace AdventOfCode.Events.Year2024
         private const char Wall = '#';
         private const char Box = 'O';
         private const char EmptySpace = '.';
+        private const string BoxWide = "[]";
         private List<StringBuilder> _warehouseMap = [];
         private List<RobotDirection> _robotMovements = [];
         (int Row, int Col) _robotPosition;
         private int MaxRow => InputLines.Count - 3;
         private int MaxCol => InputLines.First().Length - 1;
+        private int MaxColWide => (InputLines.First().Length * 2) - 1;
 
         public override long Part1()
         {
@@ -50,7 +52,41 @@ namespace AdventOfCode.Events.Year2024
 
         public override long Part2()
         {
-            throw new NotImplementedException();
+            _warehouseMap = [.. InputLines.GetRange(0, InputLines.Count - 2).Select(line => new StringBuilder(line))];
+            _warehouseMap.ToList().ForEach(line =>
+            {
+                line.Replace(Wall.ToString(), new string(Wall, 2));
+                line.Replace(EmptySpace.ToString(), new string(EmptySpace, 2));
+                line.Replace(Box.ToString(), BoxWide);
+                line.Replace(Robot.ToString(), $"{Robot}{EmptySpace}");
+            });
+            int rowRobot = _warehouseMap.ToList().FindIndex(line => line.ToString().Contains(Robot));
+            int colRobot = _warehouseMap.ElementAt(rowRobot).ToString().ToList().FindIndex(c => c == Robot);
+            _robotPosition = (rowRobot, colRobot);
+            _robotMovements = [.. InputLines.Last().Select(c => c switch
+            {
+                '^' => RobotDirection.Up,
+                '>' => RobotDirection.Right,
+                'v' => RobotDirection.Down,
+                '<' => RobotDirection.Left,
+                _ => throw new ArgumentException("What kind of direction is that...?")
+            })];
+
+            _robotMovements.ForEach(direction => PushWideBoxes(direction));
+
+            List<(int Row, int Col)> boxesCoords = [];
+            for(int row = 1; row < MaxRow; row++)
+            {
+                for(int col = 2; col < MaxColWide; col++)
+                {
+                    if (_warehouseMap[row][col] == BoxWide[0])
+                    {
+                        boxesCoords.Add((row, col));
+                    }
+                }
+            }
+
+            return boxesCoords.Sum(c => (c.Row * 100) + c.Col);
         }
 
         #region Private methods
@@ -139,6 +175,112 @@ namespace AdventOfCode.Events.Year2024
                     break;
             }
         }
+
+        private void PushWideBoxes(RobotDirection direction)
+        {
+            StringBuilder lineConfiguration;
+            switch (direction)
+            {
+                case RobotDirection.Up:
+                case RobotDirection.Down:
+                    int nextRow = direction == RobotDirection.Up ? _robotPosition.Row - 1 : _robotPosition.Row + 1;
+                    if(nextRow > 0 && nextRow < MaxRow && CanPushVertical(nextRow, _robotPosition.Col, direction))
+                    {
+                        PushBoxesVertical(nextRow, _robotPosition.Col, direction);
+
+                        _warehouseMap[nextRow][_robotPosition.Col] = Robot;
+                        _warehouseMap[_robotPosition.Row][_robotPosition.Col] = EmptySpace;
+                        if(direction == RobotDirection.Up)
+                        {
+                            _robotPosition.Row--;
+                        }
+                        else
+                        {
+                            _robotPosition.Row++;
+                        }
+                    }
+                        break;
+                case RobotDirection.Right:
+                    lineConfiguration = new StringBuilder(_warehouseMap[_robotPosition.Row].ToString()[(_robotPosition.Col + 1)..]);
+                    if(lineConfiguration.ToString().First() == EmptySpace)
+                    {
+                        _warehouseMap[_robotPosition.Row][_robotPosition.Col + 1] = Robot;
+                        _warehouseMap[_robotPosition.Row][_robotPosition.Col] = EmptySpace;
+                        _robotPosition.Col++;
+                    }
+                    else if(lineConfiguration.ToString().Contains(EmptySpace)
+                        && lineConfiguration.ToString().IndexOf(EmptySpace) < lineConfiguration.ToString().IndexOf(Wall))
+                    {
+                        _warehouseMap[_robotPosition.Row].Remove(lineConfiguration.ToString().IndexOf(EmptySpace) + _robotPosition.Col + 1, 1);
+                        _warehouseMap[_robotPosition.Row].Insert(_robotPosition.Col, EmptySpace);
+                        _robotPosition.Col++;
+                    }
+                    break;
+                case RobotDirection.Left:
+                    lineConfiguration = new StringBuilder(_warehouseMap[_robotPosition.Row].ToString()[..(_robotPosition.Col)]);
+                    if(lineConfiguration.ToString().Last() == EmptySpace)
+                    {
+                        _warehouseMap[_robotPosition.Row][_robotPosition.Col - 1] = Robot;
+                        _warehouseMap[_robotPosition.Row][_robotPosition.Col] = EmptySpace;
+                        _robotPosition.Col--;
+                    }
+                    else if(lineConfiguration.ToString().Contains(EmptySpace)
+                        && lineConfiguration.ToString().LastIndexOf(EmptySpace) > lineConfiguration.ToString().LastIndexOf(Wall))
+                    {
+                        _warehouseMap[_robotPosition.Row].Remove(lineConfiguration.ToString().LastIndexOf(EmptySpace), 1);
+                        _warehouseMap[_robotPosition.Row].Insert(_robotPosition.Col, EmptySpace);
+                        _robotPosition.Col--;
+                    }
+                    break;
+            }
+        }
+
+        private bool CanPushVertical(int row, int col, RobotDirection robotDirection)
+        {
+            if(row <= 0 || row >= MaxRow)
+            {
+                return false;
+            }
+            int nextRow = robotDirection switch
+            {
+                RobotDirection.Up => row - 1,
+                RobotDirection.Down => row + 1,
+                _ => throw new ArgumentException("This method verifies only Up or Down")
+            };
+
+            char contentNextCell = _warehouseMap[row][col];
+
+            if (contentNextCell == Wall)
+            {
+                return false;
+            }
+            else if (contentNextCell == EmptySpace)
+            {
+                return true;
+            }
+
+            int colStartBox = (contentNextCell == BoxWide[0]) ? col : col - 1;
+            return CanPushVertical(nextRow, colStartBox, robotDirection)
+                && CanPushVertical(nextRow, colStartBox + 1, robotDirection);
+        }
+
+        private void PushBoxesVertical(int row, int col, RobotDirection robotDirection)
+        {
+            char contentCell = _warehouseMap[row][col];
+            if(_warehouseMap[row][col] is Wall or EmptySpace)
+            {
+                return;
+            }
+
+            int nextRow = robotDirection == RobotDirection.Up ? row - 1 : row + 1;
+            int colStartBox = (contentCell == BoxWide[0]) ? col : col - 1;
+            PushBoxesVertical(nextRow, colStartBox, robotDirection);
+            PushBoxesVertical(nextRow, colStartBox + 1, robotDirection);
+
+            _warehouseMap[nextRow].Remove(colStartBox, 2).Insert(colStartBox, BoxWide);
+            _warehouseMap[row].Remove(colStartBox, 2).Insert(colStartBox, $"{EmptySpace}{EmptySpace}");
+        }
+
         private enum RobotDirection
         {
             Up,
